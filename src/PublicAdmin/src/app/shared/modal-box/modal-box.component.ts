65226a2e329ef9c5328516e5e6753119fb7d1965
Michal Halabica <m.halabica@gmail.com>
2023-06-24 10:47:41 +0200

    Fixed modals bug in the public admin

--- src/PublicAdmin/src/app/shared/modal-box/modal-box.component.ts
+++ src/PublicAdmin/src/app/shared/modal-box/modal-box.component.ts
@@ -12,6 +12,8 @@ export class ModalBoxComponent implements OnInit {
 
     description: ModalDescription | null;
     visible = false;
+    customComponent: any | null;
+    customComponentInjector: Injector | null;
 
     constructor(
         private modalBoxService: ModalBoxService,
@@ -41,35 +43,6 @@ export class ModalBoxComponent implements OnInit {
         };
     }
 
-    get customComponent(): any {
-        if (this.description.type === 'component') {
-            return (this.description as CustomComponentModal).component;
-        }
-
-        if (this.description.type === 'question-component') {
-            return (this.description as CustomQuestionModal).component;
-        }
-
-        return null;
-    }
-
-    get customComponentInjector(): Injector {
-        let data: any = null;
-
-        if (this.description.type === 'component') {
-            data = (this.description as CustomComponentModal).componentData;
-        }
-
-        if (this.description.type === 'question-component') {
-            data = (this.description as CustomQuestionModal).component;
-        }
-
-        return data === null ? null : Injector.create({
-            parent: this.rootInjector,
-            providers: [{ provide: DATA_INJECTION_TOKEN, useValue: data }]
-        });
-    }
-
     @HostListener('document:keydown.escape', ['$event'])
     onKeyDownNegative(_: any): void {
         this.action(null);
@@ -86,12 +59,50 @@ export class ModalBoxComponent implements OnInit {
 
     render(description: ModalDescription): void {
         this.description = description;
+
+        if (description.isCustom) {
+            this.customComponent = this.createCustomComponent();
+            this.customComponentInjector = this.createCustomComponentInjector();
+        }
+
         this.visible = true;
     }
 
     action(value: any): void {
-        this.description.onModalClose.emit(value);
-        this.description = null;
         this.visible = false;
+
+        if (this.description) {
+            this.description.onModalClose.emit(value);
+            this.description = null;
+        }
+    }
+
+    private createCustomComponent(): any {
+        if (this.description.type === 'component') {
+            return (this.description as CustomComponentModal).component;
+        }
+
+        if (this.description.type === 'question-component') {
+            return (this.description as CustomQuestionModal).component;
+        }
+
+        return null;
+    }
+
+    private createCustomComponentInjector(): Injector {
+        let data: any = null;
+
+        if (this.description.type === 'component') {
+            data = (this.description as CustomComponentModal).componentData;
+        }
+
+        if (this.description.type === 'question-component') {
+            data = (this.description as CustomQuestionModal).component;
+        }
+
+        return data === null ? null : Injector.create({
+            parent: this.rootInjector,
+            providers: [{ provide: DATA_INJECTION_TOKEN, useValue: data }]
+        });
     }
 }
