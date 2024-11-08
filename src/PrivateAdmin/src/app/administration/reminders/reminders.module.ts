7771ed4cf4f1036821d6f2f2de9cca1812a8475e
Michal Halabica <m.halabica@gmail.com>
2024-11-08 23:00:18 +0100

    Dropped support for old reminders

--- src/PrivateAdmin/src/app/administration/reminders/reminders.module.ts
+++ src/PrivateAdmin/src/app/administration/reminders/reminders.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FilterComponent } from './filter/filter.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ListComponent } from './list/list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailModalComponent } from './detail-modal/detail-modal.component';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent }
 ];
 
 @NgModule({
-    declarations: [
-        FilterComponent,
-        DashboardComponent,
-        ListComponent,
-        DetailModalComponent
-    ],
+    declarations: [DashboardComponent,],
     imports: [
         SharedModule,
         RouterModule.forChild(routes)
