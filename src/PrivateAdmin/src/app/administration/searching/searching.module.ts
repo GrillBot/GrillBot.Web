e300633f48daafc43cbc3e608573c23e411412c8
Michal Halabica <m.halabica@gmail.com>
2024-12-01 11:14:19 +0100

    Dropped support for old searching

--- src/PrivateAdmin/src/app/administration/searching/searching.module.ts
+++ src/PrivateAdmin/src/app/administration/searching/searching.module.ts
@@ -1,25 +1,13 @@
 import { NgModule } from '@angular/core';
-import { ListComponent } from './list/list.component';
-import { FilterComponent } from './filter/filter.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { SearchingDetailComponent } from './searching-detail/searching-detail.component';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-    { path: '', component: DashboardComponent }
-];
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-    declarations: [
-        ListComponent,
-        FilterComponent,
-        DashboardComponent,
-        SearchingDetailComponent
-    ],
+    declarations: [DashboardComponent],
     imports: [
         SharedModule,
-        RouterModule.forChild(routes)
+        RouterModule.forChild([{ path: '', component: DashboardComponent }])
     ]
 })
 export class SearchingModule { }
